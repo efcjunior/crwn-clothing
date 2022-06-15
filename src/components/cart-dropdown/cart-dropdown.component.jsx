8bd982735a7feb661e04328a832eaaaa2f4d3fa1
Everson Jr <everson.c.junior@gmail.com>
2022-06-14 22:19:37 -0300

    add checkout page

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,12 +1,28 @@
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { CartContext } from "../../contexts/cart.context";
 import Button from "../button/button.component";
+import CartItem from "../cart-item/cart-item.component";
 import "./cart-dropdown.styles.scss";
 
 const CartDropdown = () => {
   console.log("CartDropdown");
+  const { cartItems } = useContext(CartContext);
+  const navigate = useNavigate();
+
+
+  const goCheckoutHandler = () => {
+    navigate('/checkout');
+  }
+
   return (
     <div className="cart-dropdown-container">
-      <div className="cart-items"></div>
-      <Button>GO TO CHECKOUT</Button>
+      <div className="cart-items">
+        {cartItems.map((item) => (
+          <CartItem key={item.id} cartItem={item} />
+        ))}
+      </div>
+      <Button onClick={goCheckoutHandler}>GO TO CHECKOUT</Button>
     </div>
   );
 };
--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -6,14 +6,14 @@ import "./cart-icon.styles.scss";
 const CartIcon = () => {
   console.log("CartIcon");
 
-  const { isCartOpen, setCartOpen } = useContext(CartContext);
+  const { isCartOpen, setCartOpen, cartCount } = useContext(CartContext);
 
   const toggleIsCartOpen = () => setCartOpen(!isCartOpen);
 
   return (
     <div className="cart-icon-container" onClick={toggleIsCartOpen}>
       <ShoppingIcon className="shopping-icon" />
-      <span className="item-count">0</span>
+      <span className="item-count">{cartCount}</span>
     </div>
   );
 };
